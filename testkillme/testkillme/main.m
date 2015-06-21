fe977dc9245280c14a9f58cd7be06102d7e0d11b
Andy Wallace <andymasteroffish@gmail.com>
2015-06-21 10:42:41 -0400

    huh?

---
+++ testkillme/testkillme/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testkillme
+//
+//  Created by Andy Wallace on 6/21/15.
+//  Copyright (c) 2015 Andy Wallace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
