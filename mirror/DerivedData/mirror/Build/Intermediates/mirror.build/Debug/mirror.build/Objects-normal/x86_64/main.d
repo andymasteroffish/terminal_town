641046b3110ff22c6c29f3455d6d158ad9c80ebc
Andy Wallace <andymasteroffish@gmail.com>
2015-06-21 10:32:35 -0400

    basic mirror

---
+++ mirror/DerivedData/mirror/Build/Intermediates/mirror.build/Debug/mirror.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/andywallace/Documents/c_plus_plus_projects/terminal_town/mirror/mirror/main.cpp
---
+++ mirror/mirror/main.cpp
@@ -0,0 +1,129 @@
+//
+//  main.m
+//  arument_test
+//
+//  Created by Andy Wallace on 6/14/15.
+//  Copyright (c) 2015 Andy Wallace. All rights reserved.
+//
+
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+string reverse(string orig);
+char * reverse(const char * orig, int length);
+
+void readFile(string filePath);
+
+string getFileNameFromFullPath(string fullPath);
+string getDirectoryFromFullPath(string fullPath);
+
+
+int main(int argc, const char * argv[]) {
+    
+    //cout<<"hello "<<argc<<endl;
+    
+    
+    if (argc >= 2){
+        
+        //cout<<reverse(argv[1])<<endl;
+        readFile(string(argv[1]));
+        
+    }
+    
+    return 0;
+}
+
+void readFile(string filePath){
+    streampos size;
+    char * memblock;
+    
+    
+    ifstream file (filePath, ios::in|ios::binary|ios::ate);
+    if (file.is_open())
+    {
+        size = file.tellg();
+        memblock = new char [size];
+        file.seekg (0, ios::beg);
+        file.read (memblock, size);
+        file.close();
+        
+        //reverse it
+        char * reverseMem = reverse(memblock, size);
+        
+        //get the path
+        cout<<endl<<getFileNameFromFullPath(filePath)<<endl<<getDirectoryFromFullPath(filePath)<<endl;
+        string newFilePath = getDirectoryFromFullPath(filePath) + reverse(getFileNameFromFullPath(filePath));
+        
+        //save that shit
+        ofstream myfile (newFilePath);
+        if (myfile.is_open())
+        {
+            for (int i=0; i<size; i++){
+                myfile << reverseMem[i];
+                //cout<< (int)reverseMem[i] <<",";
+            }
+            myfile.close();
+        }
+        
+        delete[] reverseMem;
+        delete[] memblock;
+    }
+    else cout << "Unable to open file";
+}
+
+
+string reverse(string orig){
+    string returnVal = "";
+    
+    for (int i=orig.size()-1; i>=0; i--){
+        returnVal += orig[i];
+    }
+    
+    return returnVal;
+}
+
+char * reverse(const char * orig, int length){
+    
+    if (length == 0){
+        string orig_as_string = string(orig);
+        length = orig_as_string.length();
+    }
+    
+    
+    char * returnVal = new char[length];
+    
+    
+    for (int i=length-1; i>=0; i--){
+        returnVal[length-i-1] = orig[i];
+    }
+    return returnVal;
+    
+}
+
+
+string getFileNameFromFullPath(string fullPath){
+    for (int i=fullPath.size()-1; i>=0; i--){
+        if (fullPath[i] == '/'){
+            return fullPath.substr(i+1);
+        }
+    }
+    cout<<"probably a fuk up: getFileNameFromFullPath"<<endl;
+    return "";
+}
+
+string getDirectoryFromFullPath(string fullPath){
+    for (int i=fullPath.size()-1; i>=0; i--){
+        if (fullPath[i] == '/'){
+            return fullPath.substr(0,i+1);
+        }
+    }
+    cout<<"probably a fuk up: getDirectoryFromFullPath"<<endl;
+    return "";
+}
+
+
+
+
