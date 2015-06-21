a1dc0bbd811a94f16ed119fd012e1daded8853a2
Andy Wallace <andymasteroffish@gmail.com>
2015-06-21 14:16:54 -0400

    pulled general tools outside of any individual project

--- mirror/DerivedData/mirror/Build/Intermediates/mirror.build/Debug/mirror.build/Objects-normal/x86_64/main.d
+++ mirror/DerivedData/mirror/Build/Intermediates/mirror.build/Debug/mirror.build/Objects-normal/x86_64/main.d
@@ -1,3 +1,3 @@
 dependencies: \
   /Users/andywallace/Documents/c_plus_plus_projects/terminal_town/mirror/mirror/main.cpp \
-  /Users/andywallace/Documents/c_plus_plus_projects/terminal_town/mirror/mirror/tools.h
+  /Users/andywallace/Documents/c_plus_plus_projects/terminal_town/shared_tools/tools.h
